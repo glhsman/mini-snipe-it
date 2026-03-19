86aa34979c4541155843d5e964ff8d931c987ddd
Frank Schoenbrodt <frank.schoenbrodt@web.de>
2026-03-19 17:55:45 +0100

    Remove private SQL query files from repository

--- Abfrage_for_sample_location_csv.sql
+++
@@ -1,3 +0,0 @@
-SELECT g.name_of as name, CONCAT_WS(' ', strasse, hausnummer) AS address,
-CONCAT_WS(' ', plz, ort) AS city, gfgh_kuerzel as kuerzel
-FROM gfgh g;
--- Abfrage_for_sample_user_csv.sql
+++
@@ -1,5 +0,0 @@
- -- User export---
- SELECT u.winname as username, u.email, u.vname as first_name, u.zname as last_name, g.name_of as location_name 
- FROM user u
- INNER JOIN gfgh g ON g.gfghid=u.gfgh_id
- LIMIT 25;
