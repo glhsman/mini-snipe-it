f7003ad5b90ea17e35d0852dc421df7a188cc37f
Frank Schoenbrodt <frank.schoenbrodt@web.de>
2026-03-22 10:48:46 +0100

    chore: .gitignore aktualisiert - CSV, SQL und Mobile-Ordner ausschließen
    - Ignoriere alle *.csv Dateien (Arbeitsdateien)
    - Ignoriere alle *.sql Dateien AUSSER: database.sql, db_migration.sql, db_verify.sql
    - Ignoriere Mobile_offlineFirstInventur/ (lokale Entwicklung nur)
    - Entferne SQL-Abfrage-Dateien (Abfrage_for_sample_*.sql)
    - Entferne Mobile_offlineFirstInventur/ aus Repository

--- Abfrage_for_sample_user_csv.sql
+++
@@ -1,4 +0,0 @@
- -- User export---
- SELECT u.winname as username, u.email, u.vname as first_name, u.zname as last_name, g.name_of as location_name, u.personalnr as personalnummer,u.vorgesetzter, u.aktiv as status
- FROM user u
- INNER JOIN gfgh g ON g.gfghid=u.gfgh_id;
